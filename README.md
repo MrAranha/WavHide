ec95478ad767fc3be240751265a27677ced7c9a1
MrAranha <aranha.breno53@gmail.com>
2025-05-27 18:35:24 -0300

    added cli support with installation guide

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # WavHide
- 
-Test text
+
+**WavHide** is a lightweight C++ command-line tool for hiding and extracting text messages in WAV audio files using least significant bit (LSB) steganography.
+
+# Installation
+
+```
+git clone https://github.com/MrAranha/WavHide
+cd WavHide
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+```
+
+# Usage
+
+- wavhide [options] (-h for help)
--- WavHide/wavhide.cpp
+++ WavHide/wavhide.cpp
@@ -1,27 +1,60 @@
-﻿#include "WavHide.h"
-#include "DataManipulation/WriteBinary.h"
+﻿#include "DataManipulation/WriteBinary.h"
 #include "DataManipulation/CountBytes.h"
 #include "DataManipulation/WavHeader.cpp"
 #include "FileManipulation/ContentEvaluation.h"
-
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <stdio.h>
+#include <cstdlib>
 #include <readline/readline.h>
 #include <readline/history.h>
-
 #include "DataManipulation/AllocateBytes.h"
-
 using namespace std;
-#include <stdio.h>
-
-#include <stdlib.h>
-
 #include <unistd.h>
-
+#include <fstream>
 
 int main(int argc, char *argv[]) {
-	/*
+	int opt;
+	bool compileMode = false;
+	bool decodeMode = false;
+
+	while ((opt = getopt(argc, argv, "hcd")) != -1) {
+		switch (opt) {
+			case 'h':
+				return 0;
+			case 'c':
+				compileMode = true;
+			break;
+			case 'd':
+				decodeMode = true;
+			break;
+			default:
+				std::cerr << "Invalid option.\nUse -h for help.\n";
+			return EXIT_FAILURE;
+		}
+	}
+
+	if (compileMode && decodeMode) {
+		std::cerr << "Cannot use -c and -d together.\nUse -h for help.\n";
+		return EXIT_FAILURE;
+	}
+
+	if (!compileMode && !decodeMode) {
+		std::cerr << "No mode selected.\nUse -h for help.\n";
+		return EXIT_FAILURE;
+	}
+
+	if (compileMode) {
+		// compile logic here
+	} else if (decodeMode) {
+		// decode logic here
+	}
+
+	return 0;
+}
+
+
+/*
+int main(int argc, char *argv[]) {
 	int opt;
 	while ((opt = getopt(argc, argv, "hvf:")) != -1) {
 		switch (opt) {
@@ -38,7 +71,7 @@ int main(int argc, char *argv[]) {
 				fprintf(stderr, "Usage: %s [-h] [-v] [-f filename]\n", argv[0]);
 			exit(EXIT_FAILURE);
 		}
-	}*/
+	}
 
 	const char filePath[] = "/home/aranha/Documents/WavHideFiles/resultteste.wav";
 	const int fileBytes = CountBytes::Count(filePath);
@@ -61,7 +94,7 @@ int main(int argc, char *argv[]) {
 	/*std::ifstream textFile("/home/aranha/Documents/WavHideFiles/private_key.pem"); // Open in text mode (default)
 	std::string content((std::istreambuf_iterator<char>(textFile)), std::istreambuf_iterator<char>());
 	WriteBinary::WriteFile(ContentEvaluation::WriteTextOnWav(file, fileBytes, content.c_str()), fileBytes, "/home/aranha/Documents/WavHideFiles/resultteste.wav");
-	//*/
+	//
 	//Write result
 	std::string SSHKEY = ContentEvaluation::ExtractMessageFromWav(file, fileBytes);
 	std::ofstream out("/home/aranha/Documents/WavHideFiles/resultteste.pem");
@@ -69,3 +102,4 @@ int main(int argc, char *argv[]) {
 	out.close();
 	return 0;
 }
+*/
